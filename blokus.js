4cc1384f4e6775de02a4d0461535ad7544accd33
irori <irorin@gmail.com>
2011-04-10 13:58:48 +0900

    Show opponent's pieces.

--- blokus.js
+++ blokus.js
@@ -62,35 +62,92 @@ function createPiece(x, y, id, dir) {
 }
 
 function createPieces() {
-    var table = [
-	[1, 1, 0],
-	[5, 1, 0],
-	[9, 1, 0],
-	[13, 1, 0],
-	[16, 2, 0],
-	[21, 1, 0],
-	[24, 1, 0],
-	[1, 5, 0],
-	[4, 5, 0],
-	[7, 5, 2],
-	[12, 5, 2],
-	[18, 5, 2],
-	[23, 5, 0],
-	[0, 8, 0],
-	[4, 8, 2],
-	[8, 9, 2],
-	[13, 8, 2],
-	[16, 9, 0],
-	[20, 9, 2],
-	[23, 8, 0],
-	[25, 9, 0],
+    var table = [ // x, y, dir
+	[1, 1, 0], // u
+	[5, 1, 0], // t
+	[9, 1, 0], // s
+	[13, 1, 0], // r
+	[16, 2, 0], // q
+	[21, 1, 0], // p
+	[24, 1, 0], // o
+	[1, 5, 0], // n
+	[4, 5, 0], // m
+	[7, 5, 2], // l
+	[12, 5, 2], // k
+	[18, 5, 2], // j
+	[23, 5, 0], // i
+	[0, 8, 0], // h
+	[4, 8, 2], // g
+	[8, 9, 2], // f
+	[13, 8, 2], // e
+	[16, 9, 0], // d
+	[20, 9, 2], // c
+	[23, 8, 0], // b
+	[25, 9, 0], // a
     ];
     var left = 2;
     var top = 20;
+
     for (var i = 0; i < table.length; i++) {
 	var a = table[i];
 	if (!Blokus.board.isUsed(Blokus.board.player(), i))
 	    createPiece(left + a[0], top + a[1], i, a[2]);
+    }
+}
+
+function createOpponentsPieces() {
+    var table = [ //x, y, dir
+	[1, 1, 0], // u
+	[1, 5, 0], // t
+	[1, 9, 0], // s
+	[1, 13, 0], // r
+	[0, 17, 0], // q
+	[1, 20, 0], // p
+	[4, 1, 0], // o
+	[1, 24, 2], // n
+	[1, 27, 2], // m
+	[5, 7, 0], // l
+	[5, 12, 0], // k
+	[5, 17, 0], // j
+	[1, 29, 0], // i
+	[4, 21, 0], // h
+	[1, 32, 2], // g
+	[1, 36, 2], // f
+	[5, 25, 0], // e
+	[4, 30, 0], // d
+	[5, 33, 0], // c
+	[5, 36, 2], // b
+	[3, 34, 0], // a
+    ];
+
+    for (var id = 0; id < table.length; id++) {
+	var a = table[id];
+	if (Blokus.board.isUsed(1 - Blokus.board.player(), id))
+	    continue;
+
+	var x = 8 + a[0];
+	var y = 8 + a[1];
+	var dir = a[2];
+	var s = scale * 3 >> 3;
+
+	var elem = document.createElement("div");
+	elem.setAttribute("style",
+			  "left:" + x * s + "px;" +
+			  "top:" + y * s + "px;" +
+			  "position:absolute;")
+	piece = blockSet[id].rotations[dir].piece;
+	for (var i = 0; i < piece.size; i++) {
+	    var cell = document.createElement("div");
+	    cell.setAttribute("style",
+			      "position:absolute;" +
+			      "left:" + piece.coords[i][0] * s + "px;" +
+			      "top:" + piece.coords[i][1] * s + "px;" +
+			      "width:" + s + "px;" +
+			      "height:" + s + "px;");
+	    cell.className = "block" + (1 - Blokus.board.player());
+	    elem.appendChild(cell);
+	}
+	document.body.appendChild(elem);
     }
 }
 
@@ -138,5 +195,6 @@ Blokus = {}
 function initBlokus() {
     Blokus.board = createBoard(window.location.hash.substring(1));
     createPieces();
+    createOpponentsPieces();
     updateBoardView();
 }
