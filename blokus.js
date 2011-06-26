9a207a7cdb83217c1e7a92f85a68bc86cbbea907
irori <irorin@gmail.com>
2011-06-26 11:30:31 +0900

    Add a hack to avoid chrome bug.

--- blokus.js
+++ blokus.js
@@ -46,8 +46,10 @@ function rotate(elem, dir, x, y) {
     elem.childNodes[i].style.top = rot.coords[i].y * SCALE + 'px';
   }
   if (x != undefined) {
-    elem.style.left = x - SCALE / 2 + 'px';
-    elem.style.top = y - SCALE / 2 + 'px';
+    setTimeout(function() {  // Avoid Chrome bug(?)
+      elem.style.left = x - SCALE / 2 + 'px';
+      elem.style.top = y - SCALE / 2 + 'px';
+    }, 0);
   }
 }
 
