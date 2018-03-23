bfd347532244afa631f87a4b1304ab50c771a9a7
irori <irorin@gmail.com>
2018-03-23 17:26:11 +0900

    Offline support

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,7 @@
-self.addEventListener('fetch', (event) => {});
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+
+// Use stale-while-revalidate for same-origin resources
+workbox.routing.registerRoute(/\//, workbox.strategies.staleWhileRevalidate());
+
+// Cache first for Google Fonts
+workbox.routing.registerRoute(/https?:\/\/fonts.(?:googleapis|gstatic).com\/css/, workbox.strategies.cacheFirst());
