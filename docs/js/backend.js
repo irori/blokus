739b6b4a05606c86ce42d8bdf26b9e914163ae13
irori <irorin@gmail.com>
2019-12-22 15:51:23 +0900

    Move document root to docs/

--- docs/js/backend.js
+++ docs/js/backend.js
@@ -2,7 +2,7 @@ import { Move } from './move.js';
 export default class WorkerBackend {
     constructor(handler) {
         this.handler = handler;
-        this.worker = new Worker('dist/hm5move.js');
+        this.worker = new Worker('hm5move.js');
         this.worker.addEventListener('message', (e) => {
             let move = new Move(e.data.move);
             console.log(e.data.nps + ' nps');
--- src/backend.ts
+++ src/backend.ts
@@ -4,7 +4,7 @@ export default class WorkerBackend {
   private worker: Worker;
 
   constructor(private handler: (move: Move) => void) {
-    this.worker = new Worker('dist/hm5move.js');
+    this.worker = new Worker('hm5move.js');
     this.worker.addEventListener('message', (e) => {
       let move = new Move(e.data.move);
       console.log(e.data.nps + ' nps');
